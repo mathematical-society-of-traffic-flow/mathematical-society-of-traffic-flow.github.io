6727d15d2dcdacefe41685e5ace24a2dfcb28727
Ryosuke Ishiwata <rishfalee@gmail.com>
2024-10-13 00:40:24 +0900

    first commit

---
+++ assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Search",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "https://mathematical-society-of-traffic-flow.github.io/search/"
+      },]
